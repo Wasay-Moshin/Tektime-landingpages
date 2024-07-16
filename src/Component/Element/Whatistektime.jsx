dfd5f9ce78e611a1995ecd1ace0b0214e1b35689
hashir7860 <hashirjanu7098@gmail.com>
2024-07-16 14:02:21 +0500

    changes

--- src/Component/Element/Whatistektime.jsx
+++ src/Component/Element/Whatistektime.jsx
@@ -86,7 +86,7 @@ function Whatistektime() {
                         </p>
                     </div>
                 </div>
-                <div className="row mt-4" data-aos="zoom-in">
+                <div className="row mt-4">
                     <div className="col-md-4 mt-3 text-center border-end">
                         <h1 className="main-heading fw-bold">
                             <CountUp end={47} suffix="%" />
