17a253d24409e167817e4ac2caa0369180a08e3e
hashir7860 <hashirjanu7098@gmail.com>
2024-07-16 13:46:39 +0500

    add marquee

--- src/Component/Element/Brands.jsx
+++ src/Component/Element/Brands.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import Marquee from "react-fast-marquee";
 
 function Brands() {
     return (
         <div className='container'>
-            <div className="row align-items-center">
+            <Marquee>
+                <div className="d-flex gap-5">
+                    <img src="Assets/logo-1.png" alt="" className="img-fluid me-3" />
+                    <img src="Assets/logo-2.png" alt="" className="img-fluid me-3" />
+                    <img src="Assets/logo-3.png" alt="" className="img-fluid me-3" />
+                    <img src="Assets/logo-4.png" alt="" className="img-fluid me-3" />
+                    <img src="Assets/logo-5.png" alt="" className="img-fluid me-3" />
+                    <img src="Assets/logo-6.png" alt="" className="img-fluid" />
+                </div>
+            </Marquee>
+            {/* <div className="row align-items-center">
                 <div className="col-md-2 col-6 mt-4">
                     <img src="Assets/logo-1.png" alt="" className="img-fluid" />
                 </div>
@@ -22,7 +33,7 @@ function Brands() {
                 <div className="col-md-2 col-6 mt-4">
                     <img src="Assets/logo-6.png" alt="" className="img-fluid" />
                 </div>
-            </div>
+            </div> */}
         </div>
     )
 }
