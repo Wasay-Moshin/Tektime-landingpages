15df1394bd527bb4a1c9b8a74f885517f8b12f2d
hashir7860 <hashirjanu7098@gmail.com>
2024-07-16 10:57:17 +0500

    changes

--- src/Component/Element/Brands.jsx
+++ src/Component/Element/Brands.jsx
@@ -3,28 +3,26 @@ import React from 'react'
 function Brands() {
     return (
         <div className='container'>
-            <marquee direction="left" loop="infinite">
-                <div className="row align-items-center">
-                    <div className="col-md-2 col-6 mt-4">
-                        <img src="Assets/logo-1.png" alt="" className="img-fluid" />
-                    </div>
-                    <div className="col-md-2 col-6 mt-4">
-                        <img src="Assets/logo-2.png" alt="" className="img-fluid" />
-                    </div>
-                    <div className="col-md-2 col-6 mt-4">
-                        <img src="Assets/logo-3.png" alt="" className="img-fluid" />
-                    </div>
-                    <div className="col-md-2 col-6 mt-4">
-                        <img src="Assets/logo-4.png" alt="" className="img-fluid" />
-                    </div>
-                    <div className="col-md-2 col-6 mt-4">
-                        <img src="Assets/logo-5.png" alt="" className="img-fluid" />
-                    </div>
-                    <div className="col-md-2 col-6 mt-4">
-                        <img src="Assets/logo-6.png" alt="" className="img-fluid" />
-                    </div>
+            <div className="row align-items-center">
+                <div className="col-md-2 col-6 mt-4">
+                    <img src="Assets/logo-1.png" alt="" className="img-fluid" />
                 </div>
-            </marquee>
+                <div className="col-md-2 col-6 mt-4">
+                    <img src="Assets/logo-2.png" alt="" className="img-fluid" />
+                </div>
+                <div className="col-md-2 col-6 mt-4">
+                    <img src="Assets/logo-3.png" alt="" className="img-fluid" />
+                </div>
+                <div className="col-md-2 col-6 mt-4">
+                    <img src="Assets/logo-4.png" alt="" className="img-fluid" />
+                </div>
+                <div className="col-md-2 col-6 mt-4">
+                    <img src="Assets/logo-5.png" alt="" className="img-fluid" />
+                </div>
+                <div className="col-md-2 col-6 mt-4">
+                    <img src="Assets/logo-6.png" alt="" className="img-fluid" />
+                </div>
+            </div>
         </div>
     )
 }
